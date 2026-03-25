037d40c36cd571a17aed992e99f8ca940597a5fd
gabbyte <igorgabriel.png@gmail.com>
2026-03-24 21:15:38 -0300

    Exercicios de condição JS

---
+++ aula12ex/ex014/script.js
@@ -0,0 +1,20 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var imagem = window.document.getElementById('imagem')
+
+    var data = new Date()
+    var hora = data.getHours()
+
+    msg.innerHTML = `Agora são ${hora} horas`
+
+    if (hora >= 0 && hora < 12) {
+        imagem.src = 'Manhã.jpg'
+        document.body.style.background = '#a4d9e5'
+    } else if (hora >= 12 && hora < 18) {
+        imagem.src = 'Tarde.jpg'
+        document.body.style.background = '#ecdbb6'
+    } else {
+        imagem.src = 'Noite.jpg'
+        document.body.style.background = '#001e26'
+    }
+}
---
+++ aula12ex/ex015/script.js
@@ -0,0 +1,8 @@
+function verificar() {
+    var nasc = window.document.getElementById('nasctxt')
+    var homem = window.document.getElementById('sexomas')
+    var mulher = window.document.getElementById('sexofem')
+    var botao = window.document.getElementById('button')
+
+    
+}
